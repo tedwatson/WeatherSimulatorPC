c1be598dab51e64e9f4a7d8e99a1ca8c8a4307a2
Ted Watson <tedwatson123@mac.com>
2017-08-11 17:09:09 -0400

    Added movement controls to camera

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour {
 
+    public GameObject cameraParent;
+
     // The camera's movement speed
-    public float speed = 1f;
+    public float rotateSpeed = 1f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,10 +16,35 @@ public class MovementController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        var x = -Input.GetAxis("Vertical") * Time.deltaTime * rotateSpeed;
+        var y = Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed;
 
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        var y = Input.GetAxis("Vertical")   * Time.deltaTime * speed;
+        cameraParent.transform.Rotate(x, y, 0);
 
-        transform.Rotate(y, x, 0);
+        var temp = transform.rotation;
+
+        cameraParent.transform.rotation = Quaternion.Euler(new Vector3(0, Quaternion.LookRotation(transform.forward).eulerAngles.y, 0));
+
+        transform.rotation = temp;
+
+        /*
+        var x = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        var y = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+
+        print(Input.GetAxis("Horizontal"));
+
+        // Get and store current x and y rotation
+        var oldx = transform.rotation.x;
+        var oldy = transform.rotation.y;
+
+        // Move x and y rotation to neutral
+        transform.Rotate(-oldx, -oldy, 0);
+
+        // Apply the transformation
+        transform.Rotate(x, y, 0);
+
+        // Add back the old x and y rotation values
+        transform.Rotate(oldx, oldy, 0);
+        */
     }
 }
